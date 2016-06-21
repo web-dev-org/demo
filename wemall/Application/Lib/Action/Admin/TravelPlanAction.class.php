5b5718d246fa04ba71f88fb00a6c5aac4aa28d51
stillwuyan <stillwuyan@126.com>
2016-06-21 22:09:11 +0800

    fix(travel): modify changing group name function

--- wemall/Application/Lib/Action/Admin/TravelPlanAction.class.php
+++ wemall/Application/Lib/Action/Admin/TravelPlanAction.class.php
@@ -13,11 +13,18 @@ class TravelPlanAction extends PublicAction {
 		$agentlist = M('agent')->select();
 		$citylist = M('city')->select();
         $starlist = array("0"=>"三星", "1"=>"四星", "2"=>"五星");
+        $blist = array("选择早餐", "酒店提供", "日式早餐");
+        $llist = array("选择午餐", "酒店提供", "日式午餐");
+        $dlist = array("选择晚餐", "酒店提供", "日式晚餐");
+        
 
 		$this->assign("agencylist", $agencylist);
 		$this->assign("agentlist", $agentlist);
 		$this->assign("citylist", $citylist);
         $this->assign("starlist", $starlist);
+        $this->assign("blist", $blist);
+        $this->assign("llist", $llist);
+        $this->assign("dlist", $dlist);
 		$this->display();
 	}
 	
@@ -29,28 +36,16 @@ class TravelPlanAction extends PublicAction {
 		$data['flight'] = $_POST['flight'];
 		$data['agencyid'] = $_POST['agencyid'];
 		$data['agentid'] = $_POST['agentid'];
-        $data['id'] = $_POST['groupid'];
 
         $travelplan = M("travelplan");
-        if ($data['id'] != '') {
-        	//$travelplan->save($data);
-        	$data['name'] = session('data')['name'];
-        	session('data', $data);
-        } else {
-            $groupmap = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
-			$count = $travelplan->where(array("begindate"=>$data['begindate'], "agencyid"=>$data['agencyid'], "agentid"=>$data['agentid']))->count();
-			$agency = M('agency')->where(array("id"=>$data['agencyid']))->find();
-			$agent = M('agent')->where(array("id"=>$data['agentid']))->find();
-			$data['name'] = sprintf("POLO %s - %s - %s - %s团", $data['begindate'], $agency['name'], $agent['name'], $groupmap[$count]);
-			//$data['id'] = $travelplan->add($data);
-			$data['id'] = '0';
-			session('data', $data);
-		}
+        $groupmap = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+    	$count = $travelplan->where(array("begindate"=>$data['begindate'], "agencyid"=>$data['agencyid'], "agentid"=>$data['agentid']))->count();
+    	$agency = M('agency')->where(array("id"=>$data['agencyid']))->find();
+    	$agent = M('agent')->where(array("id"=>$data['agentid']))->find();
+    	$data['name'] = sprintf("POLO %s - %s - %s - %s团", $data['begindate'], $agency['name'], $agent['name'], $groupmap[$count]);
+    	session('data', $data);
 
-        $result = array();
-        $result['groupid'] = $data['id'];
-        $result['groupname'] = $data['name'];
-        $this->ajaxReturn($result, "OK", 1);
+        $this->ajaxReturn($data['name'], "OK", 1);
 	}
 
     public function getHotelList() {
